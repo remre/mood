9292fa8607b30fa0c1b83b507079f28ebe1e2685
remre <basarremre@outlook.com>
2024-01-04 16:46:31 +0300

    feat(journal):add entry card

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -2,6 +2,7 @@ import EntryCard from '@/components/EntryCard'
 import NewEntryCard from '@/components/NewEntryCard'
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
+import Link from 'next/link'
 
 const getEntries = async () => {
   const user = await getUserByClerkId()
@@ -23,9 +24,13 @@ const JournalPage = async () => {
     <div className="p-10 bg-zinc-400/10 h-full">
       <h2 className="text-3xl mb-8">Journal</h2>
       <div className="grid grid-cols-3 gap-4 ">
-        <NewEntryCard></NewEntryCard>
+        <NewEntryCard />
+
         {entries.map((entry) => (
-          <EntryCard key={entry.id} entry={entry} />
+          <Link href={`/journal/${entry.id}`} key={entry.id}>
+            {' '}
+            <EntryCard entry={entry} />
+          </Link>
         ))}
       </div>
     </div>
