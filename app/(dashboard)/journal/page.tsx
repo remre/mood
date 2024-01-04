b97f669b1d0aabe0a40f30360a812910dee04fd2
remre <basarremre@outlook.com>
2024-01-04 05:43:01 +0300

    chore(journal): add layout and page view

---
+++ app/(dashboard)/journal/page.tsx
@@ -0,0 +1,23 @@
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getEntries = async () => {
+  const user = await getUserByClerkId()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+  return entries
+}
+
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log('entries', entries)
+  return <div>journal</div>
+}
+
+export default JournalPage
