fea05bae7086c416b251a45b22da834f4bd1c208
remre <basarremre@outlook.com>
2024-01-04 06:13:51 +0300

    style(journal):update layout

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,3 +1,5 @@
+import EntryCard from '@/components/EntryCard'
+import NewEntryCard from '@/components/NewEntryCard'
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 
@@ -17,7 +19,17 @@ const getEntries = async () => {
 const JournalPage = async () => {
   const entries = await getEntries()
   console.log('entries', entries)
-  return <div>journal</div>
+  return (
+    <div className="p-10 bg-zinc-400/10 h-full">
+      <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="grid grid-cols-3 gap-4 ">
+        <NewEntryCard></NewEntryCard>
+        {entries.map((entry) => (
+          <EntryCard key={entry.id} entry={entry} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default JournalPage
