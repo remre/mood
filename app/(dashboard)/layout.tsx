c7d43a073d11381f926e1de273c666cb62bebcf1
remre <basarremre@outlook.com>
2024-01-10 01:11:25 +0300

    chore(journal ) add navigation:

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,25 @@
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
-UserButton
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+]
+
 const DashboardLayout = ({ children }) => {
   return (
     <div className="h-screen w-screen relative">
       <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
         Mood
+        <div>
+          <ul>
+            {links.map((link) => (
+              <li key={link.href} className="px-2 py-6 text-xl">
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </aside>
       <div className="ml-[200px] h-full">
         <header className="h-[60px] border-b border-black/10">
