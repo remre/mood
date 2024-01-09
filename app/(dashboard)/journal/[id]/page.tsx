aa765e3a618fc79068db9ebe01f9a1e1ed814068
remre <basarremre@outlook.com>
2024-01-10 01:15:02 +0300

    feat: add search bar

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -21,36 +21,10 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id)
-  const { mood, summary, color, subject, negative } = entry?.analysis
-  const analysisData = [
-    { name: 'Subject', value: subject },
-    { name: 'Mood', value: mood },
-    { name: 'Summary', value: summary },
-    { name: 'Negative', value: negative ? 'True' : 'False' },
-  ]
+
   return (
-    <div className="h-full w-full grid grid-cols-3">
-      <div className="col-span-2">
-        <Editor entry={entry}></Editor>
-      </div>
-      <div className="border-l  border-black/10">
-        <div className=" px-6 py-10" style={{ backgroundColor: color }}>
-          <h2 className="text-2xl">Analysis</h2>
-        </div>
-        <div>
-          <ul>
-            {analysisData.map((item) => (
-              <li
-                key={item.name}
-                className="px-2 py-4  border-b  border-t boder-black/10 flex items-center justify-between"
-              >
-                <span className="text-lg font-semibold">{item.name}</span>
-                <span>{item.value}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+    <div className="h-full w-full">
+      <Editor entry={entry}></Editor>
     </div>
   )
 }
