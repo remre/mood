b24e9fc2b0c380677200cedd221efd196e840f31
remre <basarremre@outlook.com>
2024-01-15 01:15:57 +0300

    chore(search bar): fix the id problem

--- app/api/question/route.ts
+++ app/api/question/route.ts
@@ -1,23 +1,22 @@
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import { qa } from '@/utils/ai'
+// import { NextResponse } from 'next/server'
 import { NextResponse } from 'next/server'
+
 export const POST = async (request) => {
   const { question } = await request.json()
   const user = await getUserByClerkId()
-
   const entries = await prisma.journalEntry.findMany({
     where: {
       userId: user.id,
     },
     select: {
-      id: true,
       content: true,
       createdAt: true,
     },
   })
 
   const answer = await qa(question, entries)
-
-  return NextResponse.json({ data: { answer } })
+  return NextResponse.json({ data: answer })
 }
