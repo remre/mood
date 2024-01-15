e5dc42a7a2291522f29402b58e4ef8ab7e755fa5
remre <basarremre@outlook.com>
2024-01-15 22:26:52 +0300

    feat(history): add entry charts in history

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,3 +1,4 @@
+import HistoryChart from '@/components/HistoryChart'
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 
@@ -7,8 +8,8 @@ const getData = async () => {
     where: {
       userId: user.id,
     },
-    select: {
-      sentimentScore: true,
+    orderBy: {
+      createdAt: 'asc',
     },
   })
   const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
@@ -21,7 +22,14 @@ const History = async () => {
   console.log('analyses', analyses)
   console.log('avg', avg)
 
-  return <div>History: {avg}</div>
+  return (
+    <div className="w-full h-full">
+      <div>{`Avg. Sentiment ${avg}`}</div>
+      <div className="h-full w-full">
+        <HistoryChart data={analyses} />
+      </div>
+    </div>
+  )
 }
 
 export default History
