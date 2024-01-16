1603c3e08d5cf09a9e1a707396607fb4ce52fa61
remre <basarremre@outlook.com>
2024-01-16 19:25:42 +0300

    test: add initial tests for home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from '@clerk/nextjs'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link'
 
 export default async function Home() {
---
+++ setupTests.ts
@@ -0,0 +1,3 @@
+/// <reference types="vitest/globals" />
+import '@testing-library/jest-dom'
+test(home): add test file for home component
