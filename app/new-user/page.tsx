c8c2f3d782d8eb0417e3cce88528b61f3bbf9a6c
remre <basarremre@outlook.com>
2024-01-17 08:08:05 +0300

    fix(auth):improve new user sets

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,6 @@
 import { prisma } from '@/utils/db'
 import { currentUser } from '@clerk/nextjs'
+import { Prisma } from '@prisma/client'
 import { redirect } from 'next/navigation'
 
 const createNewUser = async () => {
@@ -13,12 +14,23 @@ const createNewUser = async () => {
   })
 
   if (!match) {
-    await prisma.user.create({
-      data: {
-        clerkId: user.id,
-        email: user?.emailAddresses[0].emailAddress,
-      },
-    })
+    try {
+      await prisma.user.create({
+        data: {
+          clerkId: user.id as string,
+          email: user?.emailAddresses[0].emailAddress,
+        },
+      })
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        console.error('A user with this clerkId already exists.')
+      } else {
+        throw error
+      }
+    }
   }
 
   redirect('/journal')
--- utils/auth.ts
+++ utils/auth.ts
@@ -5,7 +5,7 @@ export const getUserByClerkId = async () => {
   const { userId } = await auth()
   const user = await prisma.user.findUniqueOrThrow({
     where: {
-      clerkId: userId as string,
+      clerkId: userId ?? undefined,
     },
   })
   return user
